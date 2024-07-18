6cc52c409ffc469b8acb1effb4d0ce1f46f6c049
shivanisharma0710 <shivanibitto07@gmail.com>
2024-07-18 16:59:56 +0530

    print the values of string operator

--- magictrick.py
+++ magictrick.py
@@ -1,4 +1,4 @@
-secret_num = 6
+secret_num = 7
 secret_num += 1
 secret_num *= 2
 secret_num += 4
---
+++ stringopeartor.py
@@ -0,0 +1,14 @@
+first_name = 'tammy'
+last_name = 'horselady'
+print (first_name + " " + last_name)
+
+
+print ("4" + "5")
+print (4+5)
+
+
+print("ha"*19)
+
+
+price = "9"
+print (tota = price * 5)
