b118e4006d9b3a6795dded66a1bd99acb6a883f4
shivanisharma0710 <shivanibitto07@gmail.com>
2024-07-26 17:30:06 +0530

    nico hulkenberg exercise

---
+++ exercise.py
@@ -0,0 +1,13 @@
+first = "nico"
+last = "hulkenberg"
+full_name = first + " " + last
+print(full_name)
+
+initials = first[0] + last[0]
+print(initials)
+
+initials_2 = first[0] + "." + last[0] + "."
+print(initials_2)
+
+nickname = last[:4]
+print(nickname)
