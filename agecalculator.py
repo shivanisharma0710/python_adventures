41cff47d25f81c42645c4e17835bfeeac72b44ba
shivanisharma0710 <shivanibitto07@gmail.com>
2024-08-02 21:57:31 +0530

    age calculator

---
+++ agecalculator.py
@@ -0,0 +1,3 @@
+age = input("how old are you (in years) ?")
+days = float(age) * 365
+print (f"{age} years is {days} days old")
--- stringfunc.py
+++ stringfunc.py
@@ -8,4 +8,4 @@ print("***WELCOME TO THE NAME APP***")
 first_name= input("what is your first name?")
 last_name= input("what is your last name?")
 full_name = first_name + " " + last_name
-print("hi there, " + full_name +"!!!")
+print("Hi there, " + full_name +"!!!")
